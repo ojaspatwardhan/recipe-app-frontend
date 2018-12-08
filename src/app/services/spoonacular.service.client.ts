7b2bd49f0e752d19402d426765211123e10e8724
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-12-08 16:11:22 -0500

    spoonacular changes

--- src/app/services/spoonacular.service.client.ts
+++ src/app/services/spoonacular.service.client.ts
@@ -4,6 +4,18 @@ export class SpoonacularServiceClient {
       headers: {"X-RapidAPI-Key": "d40a5fb0e6msh1957b3cdc0d3bbdp1785cfjsn2ce4b2e7c8bb"}
     }).then((response) => response.json());
   }
+
+  getNutritionByRecipe(id) {
+      return fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+id+"/nutritionWidget?defaultCss=true", {
+        headers: {"X-RapidAPI-Key": "d40a5fb0e6msh1957b3cdc0d3bbdp1785cfjsn2ce4b2e7c8bb"}
+      }).then((response) => response.text());
+    }
+
+    getRecipeById(id){
+      return fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+id+"/information", {
+        headers: {"X-RapidAPI-Key": "d40a5fb0e6msh1957b3cdc0d3bbdp1785cfjsn2ce4b2e7c8bb"}
+      }).then((response) => response.json());
+    }
 }
 
 /*
